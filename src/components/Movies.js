c2feeed57391f1b5e2be3aaf5346dc5b1087823f
iamvpa <vpacorp@hotmail.com>
2022-02-05 22:00:29 +0530

    added trending movies center

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,11 +4,26 @@ function Movies() {
     return (
         <>
             <div className="mb-8">
-                <div className="mt-8 text-2xl font-bold text-center">Trending Movies</div>
-                <div className={`bg-[url(${Image})] h-[30vh] w-[200px] bg-center bg-cover rounded-xl `}>
+                <div className="mt-8 mb-8 text-2xl font-bold text-center">Trending Movies</div>
+                <div className="flex flex-wrap justify-center">
+                <div className={`bg-[url(${Image})] h-[25vh] w-[150px] bg-center bg-cover rounded-xl flex items-end m-4 hover:scale-110 ease-out duration-300`}>
              
-             <div ></div>
-             </div>   
+             <div className="w-full bg-grey-500 text-white py-2 text-center rounded-b-xl">Title</div>
+             </div>
+             <div className={`bg-[url(${Image})] h-[25vh] w-[150px] bg-center bg-cover rounded-xl flex items-end m-4 hover:scale-110 ease-out duration-300`}>
+             
+             <div className="w-full bg-grey-500 text-white py-2 text-center rounded-b-xl">Title</div>
+             </div>
+             <div className={`bg-[url(${Image})] h-[25vh] w-[150px] bg-center bg-cover rounded-xl flex items-end m-4 hover:scale-110 ease-out duration-300`}>
+             
+             <div className="w-full bg-grey-500 text-white py-2 text-center rounded-b-xl">Title</div>
+             </div>
+             <div className={`bg-[url(${Image})] h-[25vh] w-[150px] bg-center bg-cover rounded-xl flex items-end m-4 hover:scale-110 ease-out duration-300`}>
+             
+             <div className="w-full bg-grey-500 text-white py-2 text-center rounded-b-xl">Title</div>
+             </div>
+                </div>
+                   
                 </div>
             
         </>
