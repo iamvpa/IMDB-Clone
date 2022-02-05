72ac8d62b11cb5bb8a12c499682a1921b535e724
iamvpa <vpacorp@hotmail.com>
2022-02-05 21:31:07 +0530

    banner completed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,11 +3,11 @@ import Image from '../banner.jpg'
 function Banner() {
     return (
         <>
-         <div className={`bg-[url(${Image})] h-[60vh] bg-center bg-cover`}>
+         <div className={`bg-[url(${Image})] h-[60vh] bg-center bg-cover flex items-end`}>
              
-             
+             <div className="text-3xl text-white p-4 bg-gray-900 bg-opacity-50 w-full flex justify-center">Spider-Man : No Way Home</div>
              </div>   
-             <h2>Hello Banner</h2>
+             
         </>
     )
 }
