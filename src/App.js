4f38564fd6020b59f6d42f537cab687c77dd3ec1
iamvpa <vpacorp@hotmail.com>
2022-02-05 21:51:24 +0530

    added movies card

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 import './App.css';
 import NavBar from './components/NavBar.js';
 import Banner from './components/Banner.js';
-
+import Movies from './components/Movies.js'
 
 function App() {
   return (<>
   <h1>Hello World!</h1>
   <NavBar></NavBar>
   <Banner></Banner>
+  <Movies></Movies>
   </>
     
   );
---
+++ src/components/Movies.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from '../banner.jpg'
+function Movies() {
+    return (
+        <>
+            <div className="mb-8">
+                <div className="mt-8 text-2xl font-bold text-center">Trending Movies</div>
+                <div className={`bg-[url(${Image})] h-[30vh] w-[200px] bg-center bg-cover rounded-xl `}>
+             
+             <div ></div>
+             </div>   
+                </div>
+            
+        </>
+    )
+}
+
+export default Movies
