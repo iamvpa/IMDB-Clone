2b5da2386475ac4a07467310d678d613170f228c
iamvpa <vpacorp@hotmail.com>
2022-02-06 21:01:08 +0530

    changes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React ,{useState,useEffect} from 'react'
 import Image from '../banner.jpg'
+import axios from 'axios'
 function Banner() {
+    const [movie,setMovie]=useState({})
+    useEffect(function(){
+        axios.get("https://api.themoviedb.org/3/trending/movie/week?api_key=7602f6a7075922c7cdd8b2b2537c49ca&page=2").then((res)=>{setMovie(res.data.results[0])})
+    },[])
+
     return (
         <>
-         <div className={`bg-[url(${Image})] h-[60vh] bg-center bg-cover flex items-end`}>
+         <div className={`bg-[url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})] h-[60vh] bg-center bg-cover flex items-end`}>
              
-             <div className="text-3xl text-white p-4 bg-gray-900 bg-opacity-50 w-full flex justify-center">Spider-Man : No Way Home</div>
+             <div className="text-3xl text-white p-4 bg-gray-900 bg-opacity-50 w-full flex justify-center">{movie.title}</div>
              </div>   
              
         </>
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,31 +1,40 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import Image from '../banner.jpg'
 import axios from 'axios'
+import {Grid} from 'react-loader-spinner'
 function Movies() {
-    axios.get("https://api.themoviedb.org/3/trending/movie/week?api_key=7602f6a7075922c7cdd8b2b2537c49ca&page=2").then((res)=>console.table(res.data.results))
+    const [movies,setMovies]=useState([])
+    useEffect(function(){
+        axios.get("https://api.themoviedb.org/3/trending/movie/week?api_key=7602f6a7075922c7cdd8b2b2537c49ca&page=2").then((res)=>{setMovies(res.data.results)})
+    })
+   
     return (
         <>
             <div className="mb-8">
                 <div className="mt-8 mb-8 text-2xl font-bold text-center">Trending Movies</div>
+                {
+                    movies.length==0?<div className="flex justify-center"><Grid
+                    heigth="100"
+                    width="100"
+                    color='grey'
+                    ariaLabel='loading'
+                  /></div>:
+                
                 <div className="flex flex-wrap justify-center">
-                <div className={`bg-[url(${Image})] h-[25vh] w-[150px] bg-center bg-cover rounded-xl flex items-end m-4 hover:scale-110 ease-out duration-300`}>
+                    {
+                        movies.map((movie)=>(
+
+                            <div className={`bg-[url(https://image.tmdb.org/t/p/w500/${movie.backdrop_path})] h-[25vh] w-[150px] bg-center bg-cover rounded-xl flex items-end m-4 hover:scale-110 ease-out duration-300`}>
              
-             <div className="w-full bg-grey-500 text-white py-2 text-center rounded-b-xl">Title</div>
-             </div>
-             <div className={`bg-[url(${Image})] h-[25vh] w-[150px] bg-center bg-cover rounded-xl flex items-end m-4 hover:scale-110 ease-out duration-300`}>
+                            <div className="w-full bg-gray-900 text-white  py-2 text-center rounded-b-xl opacity-70 ">{movie.title}</div>
+                            </div>
+
+                        ))
+                    }
+               
              
-             <div className="w-full bg-grey-500 text-white py-2 text-center rounded-b-xl">Title</div>
-             </div>
-             <div className={`bg-[url(${Image})] h-[25vh] w-[150px] bg-center bg-cover rounded-xl flex items-end m-4 hover:scale-110 ease-out duration-300`}>
-             
-             <div className="w-full bg-grey-500 text-white py-2 text-center rounded-b-xl">Title</div>
-             </div>
-             <div className={`bg-[url(${Image})] h-[25vh] w-[150px] bg-center bg-cover rounded-xl flex items-end m-4 hover:scale-110 ease-out duration-300`}>
-             
-             <div className="w-full bg-grey-500 text-white py-2 text-center rounded-b-xl">Title</div>
-             </div>
                 </div>
-                   
+}
                 </div>
             
         </>
