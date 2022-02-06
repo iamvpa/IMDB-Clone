9d6786af1ca2779a7b837c76cdd1a9761db4e5a2
iamvpa <vpacorp@hotmail.com>
2022-02-06 20:11:57 +0530

    added api integration

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import logo from '../movieslogo.png'
+import {Link} from 'react-router-dom'
 function navbar() {
     return (
         <>
         <div className="border px-8 flex space-x-8 items-center">
           <img src={logo} width="60" height="60">
           </img>
-          <div className="text-black-400 font-bold text-xl md:text-2xl">Movies</div>
-          <div className="text-black-400 font-bold text-xl md:text-2xl">Favourites</div>
+          <Link to="/" className="text-black-400 font-bold text-xl md:text-2xl">Movies</Link>
+          <Link to="/favourites" className="text-black-400 font-bold text-xl md:text-2xl">Favourites</Link>
           </div>
         </>
     )
