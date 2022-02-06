928ba9cbcbb3c0068c6f012e2614881812cfc467
iamvpa <vpacorp@hotmail.com>
2022-02-06 21:00:56 +0530

    dynamic ui

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -10,10 +10,10 @@ function Pagination() {
        setPage(page-1);
    }
   return (<>
-  <div classname="w-full flex justify-center">\
-  <button className="p-2 border-2 border-indigo-500 text-indigo-500 border-r-0 rounded-l-xl" onClick={goBack}>Previous</button>
-  <button className="p-2 border-2 border-indigo-500 text-indigo-500 bg-gray-300">{page}</button>
-  <button className="p-2 border-2 border-indigo-500 text-indigo-500 rounded-r-xl border-l-0" onClick={goAhead}>Next</button>
+  <div classname="w-full">
+  <button className="p-2 border-2 border-indigo-500 text-indigo-500 border-r-0 rounded-l-xl " onClick={goBack}>Previous</button>
+  <button className="p-2 border-2 border-indigo-500 text-indigo-500 bg-gray-300 ">{page}</button>
+  <button className="p-2 border-2 border-indigo-500 text-indigo-500 rounded-r-xl border-l-0  " onClick={goAhead}>Next</button>
   </div>
   
   </>);
