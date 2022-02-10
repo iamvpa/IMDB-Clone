71830b0e55d4c192e78623e5bb41c99c89cc748c
iamvpa <vpacorp@hotmail.com>
2022-02-10 20:38:17 +0530

    modified

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React,{useState} from 'react';
+import Pagination from './Pagination.js'
 
 function Favourites() {
+  const [curGenre,setCurGenre]=useState('All Genres');
   return <>
-  <div className='flex justify-center flex-wrap space-x-2 m-4'><button className='text-xl p-1 bg-blue-400 text-white rounded-xl font-bold bg-blue-400'>Genres Button</button>
-  <button className='text-xl p-1 bg-blue-400 text-white rounded-xl font-bold bg-gray-400 hover:bg-blue-400'>Genres Button</button>
-  <button className='text-xl p-1 bg-blue-400 text-white rounded-xl font-bold bg-gray-400 hover:bg-blue-400'>Genres Button</button>
-  <button className='text-xl p-1 bg-blue-400 text-white rounded-xl font-bold bg-gray-400 hover:bg-blue-400'>Genres Button</button></div>
-  <div>Inputs Container</div>
+  <div className='flex justify-center flex-wrap space-x-2 m-4'><button className='text-xl p-1 bg-blue-400 text-white rounded-xl font-bold bg-blue-400'>All Genres</button>
+  <button className='text-xl p-1 bg-blue-400 text-white rounded-xl font-bold bg-gray-400 hover:bg-blue-400'>Action</button>
+</div>
+  <div className='text-center'>
+  <input type='text' placeholder='Search' className='border border-2 text-center p-1 m-2'/>
+  <input type='number' placeholder='Rows' className='border border-2 text-center p-1 m-2'/>
+
+  </div>
+  
+ 
   <div>Table Container</div>
-  <div>Pagination</div>
-  </>;
+  <div className='mt-4'>
+<Pagination/>
+    </div>
+ </>
 }
 
 export default Favourites;
