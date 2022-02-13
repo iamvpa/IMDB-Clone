b3bf54af815431631b39795f777b84f37f66102c
iamvpa <vpacorp@hotmail.com>
2022-02-13 19:05:31 +0530

    working model

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,59 +4,106 @@ import axios from 'axios'
 import {Grid} from 'react-loader-spinner'
 import Pagination from './Pagination.js'
 function Movies() {
-    const [page,setPage]=useState(1);
-    const [hover,setHover]=useState('');
-    const [favourites,setFavourites]=useState([]);
-    function goAhead(){
-        setPage(page+1);
-    }
-    function goBack(){
-        if(page>1)
-        setPage(page-1);
-    }
-    const [movies,setMovies]=useState([])
-    useEffect(function(){
-        axios.get(`https://api.themoviedb.org/3/trending/movie/week?api_key=7602f6a7075922c7cdd8b2b2537c49ca&page=${page}`).then((res)=>{setMovies(res.data.results)})
-    },[page])
-   let add=(movie)=>{let newArray=[...favourites,movie]
-setFavourites([...newArray])}
-    return (
-        <>
-            <div className="mb-8">
-                <div className="mt-8 mb-8 text-2xl font-bold text-center">Trending Movies</div>
-                {
-                    movies.length==0?<div className="flex justify-center"><Grid
-                    heigth="100"
-                    width="100"
-                    color='grey'
-                    ariaLabel='loading'
-                  /></div>:
-                
-                <div className="flex flex-wrap justify-center">
-                    {
-                        movies.map((movie)=>(
+    const [movies, setMovies] = useState([])
+    const [hover, setHover] = useState("")
+    const [favourites, setFavourites] = useState([])
+    const [curPage, setCurPage] = useState(1)
 
-                            <div className={`bg-[url(https://image.tmdb.org/t/p/w500/${movie.backdrop_path})] h-[25vh] w-[150px] bg-center bg-cover rounded-xl flex items-end m-4 hover:scale-110 ease-out duration-300 relative`} onMouseEnter={()=>setHover(movie.id)} onMouseLeave={()=>setHover("")}>
+    useEffect(async () => {
+        let res = await axios.get(`https://api.themoviedb.org/3/trending/movie/week?api_key=5540e483a20e0b20354dabc2d66a31c9&page=${curPage}`)
+        // console.log(res.data.results)
+        setMovies(res.data.results)
+
+        let oldFavourites = JSON.parse(localStorage.getItem("imdb")) || []
+        setFavourites([...oldFavourites])
+    }, [curPage])
+
+
+    let goForward = () => {
+        setCurPage(curPage + 1)
+    }
+
+    let goBack = () => {
+        if (curPage != 1)
+            setCurPage(curPage - 1)
+    }
+
+
+    function addToFavourites(movie) {
+        let newArray = [...favourites, movie]
+        setFavourites([...newArray])
+        localStorage.setItem('imdb', (JSON.stringify(newArray)));
+    }
+
+    function removeFromFavourites(movie) {
+        let newArray = favourites.filter(function (mObj) {
+            return movie.id != mObj.id
+        })
+        setFavourites([...newArray])
+        localStorage.setItem('imdb', JSON.stringify(newArray));
+    }
+    
+        return <div className='mt-8 max-w-[100vw]'>
+        <div class="flex items-center justify-center">
+        </div>
+        <div className='font-bold text-2xl'>Trending Movies</div>
+        {/* <div class="shadow rounded-md p-4 w-[300px] mx-auto">
+      <div class="animate-pulse flex space-x-4">
+          <div class="flex-1 space-y-6 py-1">
+              <div class="h-8 bg-slate-200 rounded"></div>
+              <div class="h-8 bg-slate-200 rounded"></div>
+              <div class="h-8 bg-slate-200 rounded"></div>
+          </div>
+      </div>
+  </div> */}
+        <div className='flex flex-wrap justify-center mt-8 '>
+            {
+                movies.map((movie, index) => (
+                    <div key={movie.id} className={` shadow w-[150px] h-[25vh] md:w-[300px] md:h-[35vh] bg-gray-500 rounded-3xl m-4 relative bg-[url(https://image.tmdb.org/t/p/w500/${movie.backdrop_path})] bg-center hover:scale-110 ease-out duration-300`}
+                        onMouseEnter={() => setHover(movie.id)} onMouseLeave={() => setHover('')}
+                    >
+                        {/* {
+                            hover == movie.id ? !favourites.find((m) => m.id == movie.id ? true : false) ?
+                                <button className='p-4 bg-gray-900 rounded-xl absolute right-4 top-4' 
+                                // onClick={() => addToFavourites(movie)}
+                                >😍</button>
+                                :
+                                <button className='p-4 bg-gray-900 rounded-xl absolute right-4 top-4' 
+                                // onClick={() => removeFromFavourites(movie)}
+                                >❌</button>
+                                :
+                                <></>
+                        } */}
+                        {
+                            hover == movie.id &&
+                            <>
                                 {
-                                        hover==movie.id && <>{!favourites.find((m)=>m.id==movie.id)?<div className='absolute top-2 right-2 p-2 bg-gray-800 text-xl rounded-xl cursor-pointer' onClick={()=>add(movie)}>😍</div>: <div className='absolute top-2 right-2 p-2 bg-gray-800 text-xl rounded-xl cursor-pointer' onClick={()=>add(movie)}>❌</div>}
-                                       
-                                        
-                                        </>
+                                    favourites.find((mObj) => mObj.id == movie.id) ?
+                                        <button onClick={function () { removeFromFavourites(movie) }} className='p-2 bg-gray-900 rounded-2xl bg-opacity-80 absolute right-2 top-2'>
+                                            ❌
+                                        </button>
+                                        :
+                                        <button onClick={function () { addToFavourites(movie) }} className='p-2 bg-gray-900 rounded-2xl bg-opacity-80 absolute right-2 top-2'>
+                                            😍
+                                        </button>
                                 }
-                                
-                            <div className="w-full bg-gray-900 text-white  py-2 text-center rounded-b-xl opacity-70 ">{movie.title}</div>
-                            </div>
+                            </>
+                        }
+                        <div className='absolute bg-gray-900 h-[40px] bottom-0 w-full text-white flex justify-center items-center font-bold rounded-b-xl'>{
+                            movie.title
+                        }
 
-                        ))
-                    }
-               
-             
-                </div>
-}
-                </div>
-            <Pagination pageProp={page} goBack={goBack} goAhead={goAhead}/>
-        </>
-    )
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
+        <Pagination curPage={curPage} goBack={goBack} goForward={goForward} setCurPage={setCurPage} />
+        {/* <button className='p-4 border  border-indigo-500 text-indigo-500 border-r-0 rounded-l-lg'>Previous</button>
+  <button className='p-4 border bg-gray-200 border-indigo-500 text-indigo-500'>1</button>
+  <button className='p-4 border border-indigo-500 text-indigo-500 border-l-0 rounded-r-lg'>Next</button> */}
+    </div>
+   
 }
 
 export default Movies
